eedff458b778b9bfb4f1d146339dee2b042967e8
Luiz Carlos <luiz.rufino@academico.ifpb.edu.br>
2025-11-24 00:07:17 -0300

    Finalização da implementação das classes.

--- ProjetoPoo/src/sistema/SistemaBiblioteca.java
+++ ProjetoPoo/src/sistema/SistemaBiblioteca.java
@@ -4,25 +4,63 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SistemaBiblioteca {
+	private List<Item> itens;
 	private List<Usuario> usuarios;
 	private List<Emprestimo> emprestimos;
 	private List<ServicoComunitario> servicos;
 	
 	public SistemaBiblioteca() {
+		this.itens = new ArrayList<>();
 		this.usuarios = new ArrayList<>();
 		this.emprestimos = new ArrayList<>();
 		this.servicos = new ArrayList<>();
 	}
 	
-	public void adicionarItem() {
+	public void adicionarItem(Item item) {
 		
 	}
 	
-	public void cadastrarUsuario() {
+	public void cadastrarUsuario(Usuario usuario) {
 		
 	}
 	
-	public void cadastrarServico() {
+	public boolean registrarEmprestimo(int itemCodigo, String cpfUsuario) {
+		return false;
+	}
+	
+	public boolean registrarDevolucao(int itemCodigo, String cpfUsuario) {
+		return false;
+	}
+	
+	public String listarItens() {
+		return "";
+	}
+	
+	public String listarItensDisponiveis() {
+		return "";
+	}
+	
+	public String listarEmprestimos() {
+		return "";
+	}
+	
+	public void cadastrarServico(ServicoComunitario servico) {
 		
+	}
+	
+	public String listarServicos() {
+		return "";
+	}
+	
+	public Usuario buscarUsuarioPorCpf(String cpf) {
+		return null;
+	}
+	
+	public Item buscarItemPorCodigo(int codigo) {
+		return null;
+	}
+	
+	public boolean registrarParticipacaoServico(int idServico, String cpfUsuario) {
+		return false;
 	}
 }
